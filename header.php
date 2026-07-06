1929bfc4d96f34a42551c05b0797fac69e0f1d3b
Daniel Curran <danielcurrannh@gmail.com>
2026-07-06 19:31:50 -0400

    Added ability to delete recipe, Added source code link to cog

--- header.php
+++ header.php
@@ -8,23 +8,27 @@ $currentUser = current_user();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>myCookBook</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 <header class="site-header">
     <a class="logo" href="index.php"><img src="assets/logo.png" alt="myCookBook logo" onerror="this.src='assets/placeholder-logo.svg'"></a>
-    <nav class="user-menu">
-        <button class="cog" aria-label="User menu">⚙</button>
-        <div class="dropdown">
-            <?php if ($currentUser): ?>
-                <a href="user.php?id=<?= (int)$currentUser['fld_id'] ?>">My Page</a>
-                <a href="settings.php">Settings</a>
-                <a href="logout.php">Log out</a>
-            <?php else: ?>
-                <a href="login.php">Log in</a>
-                <a href="register.php">Register</a>
-            <?php endif; ?>
-        </div>
-    </nav>
+    <div class="header-actions">
+        <nav class="user-menu">
+            <button class="cog" aria-label="User menu">⚙</button>
+            <div class="dropdown">
+                <a href="https://github.com/danielcurrannh/myCookBook" title="Source code"><i class="fa-brands fa-square-git" aria-hidden="true"></i> Source code</a>
+                <?php if ($currentUser): ?>
+                    <a href="user.php?id=<?= (int)$currentUser['fld_id'] ?>">My Page</a>
+                    <a href="settings.php">Settings</a>
+                    <a href="logout.php">Log out</a>
+                <?php else: ?>
+                    <a href="login.php">Log in</a>
+                    <a href="register.php">Register</a>
+                <?php endif; ?>
+            </div>
+        </nav>
+    </div>
 </header>
 <main class="page <?= e($pageClass ?? '') ?>">
--- recipe.php
+++ recipe.php
@@ -13,6 +13,16 @@ $isCreator = $currentUser && (int)$currentUser['fld_id'] === (int)$recipe['fld_u
 db()->prepare('UPDATE tbl_recipes SET fld_viewCount = fld_viewCount + 1 WHERE fld_id = ?')->execute([$id]);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = require_login();
+    if (($_POST['action'] ?? '') === 'delete') {
+        if ((int)$user['fld_id'] !== (int)$recipe['fld_userId']) {
+            http_response_code(403);
+            exit('You can only delete recipes you created.');
+        }
+
+        db()->prepare('DELETE FROM tbl_recipes WHERE fld_id = ? AND fld_userId = ?')->execute([$id, (int)$user['fld_id']]);
+        redirect('user.php?id=' . (int)$user['fld_id']);
+    }
+
     db()->prepare('INSERT IGNORE INTO tbl_userSettings (fld_userId, fld_savedRecipeId) VALUES (?, ?)')->execute([(int)$user['fld_id'], $id]);
     redirect('recipe.php?id=' . $id);
 }
@@ -22,8 +32,18 @@ include __DIR__ . '/header.php';
     <img class="detail-thumb" src="<?= e($recipe['fld_thumbnailImage'] ?: 'assets/placeholder.svg') ?>" alt="Recipe thumbnail">
     <h1><?= e($recipe['fld_recipeTitle']) ?></h1>
     <p>By <a href="user.php?id=<?= (int)$recipe['fld_userId'] ?>"><?= e($recipe['fld_userName']) ?></a></p>
-    <?php if ($isCreator): ?><p><a class="button inline-button" href="recipe_edit.php?id=<?= (int)$recipe['fld_id'] ?>">Edit recipe</a></p><?php endif; ?>
-    <?php if ($currentUser): ?><form method="post"><button>Save to favorites</button></form><?php endif; ?>
+    <div class="recipe-actions">
+        <?php if ($isCreator): ?>
+            <a class="button inline-button" href="recipe_edit.php?id=<?= (int)$recipe['fld_id'] ?>">Edit recipe</a>
+            <form class="inline-form" method="post" onsubmit="return confirm('Delete this recipe permanently?');">
+                <input type="hidden" name="action" value="delete">
+                <button class="button danger-button" type="submit">Delete recipe</button>
+            </form>
+        <?php endif; ?>
+        <?php if ($currentUser): ?>
+            <form class="inline-form" method="post"><button class="button" type="submit">Save to favorites</button></form>
+        <?php endif; ?>
+    </div>
     <h2>Ingredients</h2>
     <p class="preline"><?= e($recipe['fld_recipeIngredients']) ?></p>
     <h2>Directions</h2>
